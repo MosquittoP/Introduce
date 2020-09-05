538d4869bc33045d5961257e3e507800c299e018
Yun Sangmok <55524777+MosquittoP@users.noreply.github.com>
2020-09-05 23:27:50 +0900

    component 파일 정리

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "../components/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -14,7 +14,7 @@ const routes = [
     path: "/about",
     name: "About",
     component: () =>
-      import("../views/About.vue")
+      import("../components/About.vue")
   }
 ];
 
