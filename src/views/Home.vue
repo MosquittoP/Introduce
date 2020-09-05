f3a3a3f22a4a25033c1b0bb154697b110d5b2405
Yun Sangmok <55524777+MosquittoP@users.noreply.github.com>
2020-09-05 19:24:08 +0900

    기본 멘트 변경 (error 1)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="안녕하세요 윤상목입니다" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Main from "@/components/Main.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Main
   }
 };
 </script>
